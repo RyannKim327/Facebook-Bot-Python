c84f283266855ad231b5b04f9eb802ef4d7adcca
RyannKim327 <rksesgundo123@gmail.com>
2023-01-07 14:00:12 +0800

    01-07-23 02:00

--- echo.py
+++ echo.py
@@ -1,16 +1,42 @@
 from fbchat import Client, log
+import re
 
 class EchoBot(Client):
 	selfRead = False
+	commands = []
+
+	def selfRead(self, data=False):
+		self.selfRead = data
+
+	def addCommand(self, file="", data={}):
+		self.commands.append({
+			"file": file,
+			"data": data
+		})
+	
 	def onMessage(self, mid, author_id, message, thread_id, thread_type, **kwargs):
 		print(f"Message come in {message}")
 
+		def send(msg=""):
+			self.sendMessage(msg, thread_id=thread_id, thread_type=thread_type)
+
 		if self.selfRead or author_id != self.uid:
-				self.sendMessage("This is an auto send message from fbchat", thread_id=thread_id, thread_type=thread_type)
+			for i in self.commands:
+				command = "^" + i['data']['command'] + "$"
+				if re.search(command, message):
+					send("This is an auto send message from fbchat")
 
 
 
 class Execute:
-	def __init__(self, email, password):
-		c = EchoBot(email, password)
-		c.listen()
+	def __init__(self, email="", password=""):
+		self.c = EchoBot(email, password)
+	
+	def setSelfListen(self, isSelfListen=False):
+		self.c.selfRead(isSelfListen)
+
+	def addCommand(self, file="", data={}):
+		self.c.addCommand(file, data)
+
+	def start(self):
+		self.c.listen()
--- index.py
+++ index.py
@@ -2,3 +2,11 @@ from echo import Execute
 
 # sample: a = Execute("09123456789", "Hello world")
 a = Execute("<email>", "<password>")
+
+a.setSelfListen(True)
+a.addCommand("a", {
+	"title": "Test",
+	"description": "Test Mode",
+	"command": "/test"
+})
+a.start()
